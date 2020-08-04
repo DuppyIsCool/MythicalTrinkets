30c191f5ee5ea15c6b65eed77275daeb1f29117d
Duppster <lqvalproductions@gmail.com>
2020-08-04 11:28:40 -0400

    Using Recipe.ExactChoice
    Removed usage of CraftItemEvent and instead switched to using the
    Recipe.ExactChoice.

--- MythicalTrinkets/src/me/DuppyIsCool/DiamondShards/ShardRecipe.java
+++ MythicalTrinkets/src/me/DuppyIsCool/DiamondShards/ShardRecipe.java
@@ -1,44 +1,58 @@
 package me.DuppyIsCool.DiamondShards;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.RecipeChoice.ExactChoice;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import me.DuppyIsCool.Main.Plugin;
 import net.md_5.bungee.api.ChatColor;
 
+@SuppressWarnings("deprecation")
 public class ShardRecipe {
 	
 	public static void createRecipe() {
-		//Create itemstack
+		//Create diamond Itemstack
 		ItemStack diamond = new ItemStack(Material.DIAMOND,1);
-		//Create key
-		NamespacedKey diamondkey = new NamespacedKey(Plugin.plugin, "diamondkey");
 		
-		//Form shapeless recipe
-		ShapelessRecipe diamondrecipe = new ShapelessRecipe(diamondkey, diamond);
-		
-		//Add "diamond shards" (9) to recipe
-		diamondrecipe.addIngredient(9,Material.PRISMARINE_CRYSTALS);
-		
-		//Add recipe to bukkit
-		Bukkit.addRecipe(diamondrecipe);
-		
-		//Create Itemstack
-		ItemStack shard = new ItemStack(Material.PRISMARINE_CRYSTALS,9);
+		//Create shard Itemstack
+		ItemStack shard = new ItemStack(Material.PRISMARINE_CRYSTALS,1);
 		
 		//Set meta
 		ItemMeta meta = shard.getItemMeta();
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.LIGHT_PURPLE + ""+ChatColor.ITALIC+"A small fragment of a diamond");
+		meta.setLore(lore);
 
 		meta.setDisplayName(ChatColor.BLUE + "Diamond Shard");
 
 		shard.setItemMeta(meta);
+		
+		//Recipe Choice
+		ExactChoice diamondShard = new RecipeChoice.ExactChoice(shard);
+		
+		//Create key
+		NamespacedKey diamondkey = new NamespacedKey(Plugin.plugin, "diamondkey");
+		
+		//Form shapeless recipe
+		ShapedRecipe diamondrecipe = new ShapedRecipe(diamondkey, diamond);
+		
+		//Add "diamond shards" (9) to recipe
+		diamondrecipe.shape("DDD","DDD","DDD");
+		diamondrecipe.setIngredient('D', diamondShard);
+		
+		//Add recipe to bukkit
+		Bukkit.getServer().addRecipe(diamondrecipe);
 		//Create key
 		NamespacedKey shardkey = new NamespacedKey(Plugin.plugin, "shardkey");
-		
+		shard.setAmount(9);
 		//Form shapeless recipe
 		ShapelessRecipe shardrecipe = new ShapelessRecipe(shardkey, shard);
 		shardrecipe.addIngredient(1, Material.DIAMOND);
--- MythicalTrinkets/src/me/DuppyIsCool/Main/Events.java
+++ MythicalTrinkets/src/me/DuppyIsCool/Main/Events.java
@@ -5,50 +5,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
-
-import org.bukkit.inventory.ItemStack;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class Events implements Listener{
-	
-	//Prevents Crafting of Diamonds using normal Prismarine Shards
-	@EventHandler
-	public void restrictCrafting(PrepareItemCraftEvent e) {
-		boolean check = true;
-		//Checks if recipe is finished
-		if(e.getRecipe() != null) {
-			ItemStack resultItem = e.getRecipe().getResult();
-			
-			//Check if the result is a diamond
-			if(resultItem.getType().equals(Material.DIAMOND)) {
-				
-				//Loop through the crafting grid
-				for(ItemStack item: e.getInventory().getMatrix()) {
-					//If the item exists
-					if(item != null) {
-						if(item.getType().equals(Material.PRISMARINE_CRYSTALS)) {
-							if(item.hasItemMeta()) {
-								//If the item is a prismarine shard
-								if(!item.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.BLUE + "Diamond Shard")) {
-									check = false;
-									break;
-								}
-							}
-							else {
-								check = false;
-								break;}
-						}
-					}
-				}
-				
-				if(check == false) {
-					e.getInventory().setResult(null);
-				}
-			}
-		}
-	}
 	
 	//Prevents renaming of diamond shards (to prevent griefs or loss of shards)
 	@EventHandler(priority = EventPriority.MONITOR)
